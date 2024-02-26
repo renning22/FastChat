7f8c962482ea770145aff94b15999f50de009f15
Trapile <trapile@k8s-node09>
2024-02-26 08:38:41 +0800

    finetune commands

---
+++ ning_train_llama2.sh
@@ -0,0 +1,23 @@
+torchrun --nproc_per_node=4 --master_port=20001 fastchat/train/train_mem.py \
+    --model_name_or_path meta-llama/Llama-2-7b-hf \
+    --data_path data/dummy_conversation.json \
+    --output_dir output_vicuna \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 1200 \
+    --save_total_limit 10 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --fsdp "full_shard auto_wrap" \
+    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
+    --fp16 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing False \
+    --lazy_preprocess True
---
+++ ning_train_mistral.sh
@@ -0,0 +1,22 @@
+torchrun --nproc_per_node=4 --master_port=20001 fastchat/train/train_mem.py \
+    --model_name_or_path mistralai/Mistral-7B-Instruct-v0.2 \
+    --data_path data/dummy_conversation.json \
+    --output_dir output_mistral \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 1200 \
+    --save_total_limit 10 \
+    --learning_rate 1e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --fsdp "full_shard auto_wrap offload" \
+    --fp16 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing False \
+    --lazy_preprocess True
